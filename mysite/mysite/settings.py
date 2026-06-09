3059984c98ca3ca93ff09dc3723e531b746f84ea
tharwatmella <tharwat-mella@hotmail.com>
2026-06-09 19:58:18 +0300

    Restore me for any error 2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -123,8 +123,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CSRF_TRUSTED_ORIGINS = ['https://blogo-production-1d89.up.railway.app']
+
 
 
 LOGIN_REDIRECT_URL = "/"
 LOGIN_URL = "/login/"
 CSRF_COOKIE_SECURE=True
+SESSION_COOKIE_SECURE=True
